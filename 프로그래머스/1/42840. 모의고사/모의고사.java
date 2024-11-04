ad7aa8cde7f66d908ddcd751bcdfc97904d196c7
서보현 <98081610+seobottttt@users.noreply.github.com>
2024-11-04 22:03:00 +0900

    [level 1] Title: 모의고사, Time: 1.23 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,32 +2,46 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(int[] answers) {
-        List<Integer> answer = new ArrayList<>();
-        
-        int[] first = new int[]{1,2,3,4,5};
-        int[] second = new int[]{2,1,2,3,2,4,2,5};
-        int[] third = new int[]{3,3,1,1,2,2,4,4,5,5};
+        List<Integer> answer = new ArrayList<Integer>();
+        List<Integer> points = new ArrayList<Integer>();
+
+        // 1번 12345 5주기
+        // 2번 21232425 8주기
+        // 3번 3311224455 10주기
+        int[] oneAns = new int[] {1,2,3,4,5};
+        int[] twoAns = new int[] {2,1,2,3,2,4,2,5};
+        int[] threeAns = new int[] {3,3,1,1,2,2,4,4,5,5};
         
         int one = 0;
         int two = 0;
         int three = 0;
         
         for(int i=0; i<answers.length; i++){
-            int cur = answers[i];
-            if(cur == first[i%5]) one ++;
-            if(cur == second[i%8]) two++;
-            if(cur == third[i%10]) three++;
+            if(answers[i]==oneAns[i%5]) one++;
+            if(answers[i]==twoAns[i%8]) two++;
+            if(answers[i]==threeAns[i%10]) three++;
         }
         
         int max = 0;
-        max = Math.max(one,max);
-        max = Math.max(two,max);
-        max = Math.max(three,max);
         
-        if(max == one) answer.add(1);
-        if(max == two) answer.add(2);
-        if(max == three) answer.add(3);
+        points.add(one);
+        points.add(two);
+        points.add(three);
         
+        Collections.sort(points,Collections.reverseOrder());
+        
+        if(points.get(0)==one){
+            answer.add(1);
+        }if(points.get(0)==two){
+            answer.add(2);
+        }if(points.get(0)==three){
+            answer.add(3);
+        }
+
+        
+        
+    
+       
         
         return answer;
     }
